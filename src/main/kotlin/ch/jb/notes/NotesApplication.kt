6a31db9ba397cbf23d910b7e5f68937d324e146a
Joël Bieli <joel.bieli@edu.tbz.ch>
2019-04-01 18:04:51 +0200

    Misc stuff

--- src/main/kotlin/ch/jb/notes/NotesApplication.kt
+++ src/main/kotlin/ch/jb/notes/NotesApplication.kt
@@ -1,10 +1,28 @@
 package ch.jb.notes
 
+import ch.jb.notes.note.domainmodel.ListNote
+import ch.jb.notes.note.repository.ListNoteRepository
+import ch.jb.notes.note.repository.TextNoteRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.time.LocalDateTime
+import java.util.stream.Stream
 
 @SpringBootApplication
-class NotesApplication
+class NotesApplication {
+    @Bean
+    fun run(listNoteRepository: ListNoteRepository, textNoteRepository: TextNoteRepository) = CommandLineRunner {
+        listNoteRepository.deleteAll().subscribe(null, null, {
+            Stream.of(
+                    ListNote(null, "Test Note", LocalDateTime.now(), LocalDateTime.now(), listOf("test content"))
+            ).forEach {
+                listNoteRepository.save(it).subscribe(System.out::println)
+            }
+        })
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<NotesApplication>(*args)
