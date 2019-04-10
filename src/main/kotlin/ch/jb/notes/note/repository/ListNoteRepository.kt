312f41bafc664c817dba92523ef18e21cfdffd2b
Joël Bieli <joel.bieli@edu.tbz.ch>
2019-04-10 07:24:13 +0200

    Add repo method to delete all notes of user
    Added a method to the note repositories to delete all notes of a user in
    case a user is deleted so there wont be any notes without a owner

--- src/main/kotlin/ch/jb/notes/note/repository/ListNoteRepository.kt
+++ src/main/kotlin/ch/jb/notes/note/repository/ListNoteRepository.kt
@@ -1,6 +1,9 @@
 package ch.jb.notes.note.repository
 
 import ch.jb.notes.note.domainmodel.ListNote
+import ch.jb.notes.note.domainmodel.User
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface ListNoteRepository: ReactiveMongoRepository<ListNote, String>
+interface ListNoteRepository: ReactiveMongoRepository<ListNote, String> {
+    fun deleteAllByOwner(owner: User)
+}
--- src/main/kotlin/ch/jb/notes/note/repository/TextNoteRepository.kt
+++ src/main/kotlin/ch/jb/notes/note/repository/TextNoteRepository.kt
@@ -1,6 +1,9 @@
 package ch.jb.notes.note.repository
 
-import ch.jb.notes.note.domainmodel.ListNote
+import ch.jb.notes.note.domainmodel.TextNote
+import ch.jb.notes.note.domainmodel.User
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface TextNoteRepository: ReactiveMongoRepository<ListNote, String>
+interface TextNoteRepository: ReactiveMongoRepository<TextNote, String> {
+    fun deleteAllByOwner(owner: User)
+}
