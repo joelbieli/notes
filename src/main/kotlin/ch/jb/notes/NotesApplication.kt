70c534e444acb59051d7de92df50c82f4d5862d1
Joël Bieli <joel.bieli@edu.tbz.ch>
2019-04-10 07:27:40 +0200

    Add some default notes

--- src/main/kotlin/ch/jb/notes/NotesApplication.kt
+++ src/main/kotlin/ch/jb/notes/NotesApplication.kt
@@ -1,6 +1,7 @@
 package ch.jb.notes
 
 import ch.jb.notes.note.domainmodel.ListNote
+import ch.jb.notes.note.domainmodel.TextNote
 import ch.jb.notes.note.repository.ListNoteRepository
 import ch.jb.notes.note.repository.TextNoteRepository
 import org.springframework.boot.CommandLineRunner
@@ -8,19 +9,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import java.time.LocalDateTime
-import java.util.stream.Stream
 
 @SpringBootApplication
 class NotesApplication {
+
     @Bean
     fun run(listNoteRepository: ListNoteRepository, textNoteRepository: TextNoteRepository) = CommandLineRunner {
-        listNoteRepository.deleteAll().subscribe(null, null, {
-            Stream.of(
-                    ListNote(null, "Test Note", LocalDateTime.now(), LocalDateTime.now(), listOf("test content"))
-            ).forEach {
-                listNoteRepository.save(it).subscribe(System.out::println)
-            }
-        })
+        val lnote = ListNote(
+                null,
+                "title1",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                null,
+                listOf("content 11", "content 12")
+        )
+        listNoteRepository.deleteAll().subscribe()
+        listNoteRepository.save(lnote).subscribe(null, null, { println("gucci") })
+
+        val tnote = TextNote(
+                null,
+                "title1",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                null,
+                "coooonnnteeennnt"
+        )
+        textNoteRepository.deleteAll().subscribe()
+        textNoteRepository.save(tnote).subscribe(null, null, { println("gucci") })
     }
 }
 
