d982e01aa5c9f6a0dd97e9e8f7fd09d1a1b77750
Joël Bieli <joel.bieli@edu.tbz.ch>
2019-04-28 21:32:10 +0200

    Add react router

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,11 +3,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
+import Authentication from "./components/Authentication";
 import 'antd/dist/antd.css';
+import {BrowserRouter, Route} from "react-router-dom";
 
 render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <Route exact path={'/'} component={App}/>
+            <Route exact path={'/auth'} component={Authentication}/>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
