675a140df06ad08ffc16277eea0acad62a9358cd
Joël Bieli <joel.bieli@edu.tbz.ch>
2019-05-07 15:15:09 +0200

    Add API doc config

---
+++ src/main/kotlin/ch/jb/notes/config/SwaggerConfig.kt
@@ -0,0 +1,30 @@
+package ch.jb.notes.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux
+
+@Configuration
+@EnableSwagger2WebFlux
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .enable(true)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+
+    private fun apiInfo(): ApiInfo = ApiInfoBuilder()
+            .title("Reactive Notes API documentation")
+            .version("1.0")
+            .build()
+}
---
+++ src/test/kotlin/ch/jb/notes/GenerateSwaggerDoc.kt
@@ -0,0 +1,36 @@
+package ch.jb.notes
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MockMvcBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import java.io.File
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class GenerateSwaggerDoc {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun generateSwagger() {
+        webTestClient
+                .get()
+                .uri("/v2/api-docs")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody().consumeWith { File("swagger.json").writeText(String(it.responseBody!!)) }
+    }
+}
